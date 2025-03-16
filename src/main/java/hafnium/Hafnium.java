fe1716a3592acb49a211622493889e72dc511c4b
Hugi Thordarson <hugi@karlmenn.is>
2025-03-16 13:47:49 +0000

    Properly register the element classes

--- src/main/java/hafnium/Hafnium.java
+++ src/main/java/hafnium/Hafnium.java
@@ -11,26 +11,25 @@ import hafnium.components.USViewWrapper;
 import hafnium.components.ViewLink;
 import hafnium.urls.ObjectRouteHandler;
 import ng.appserver.NGApplication;
-import ng.appserver.templating.NGElementUtils;
 import ng.plugins.NGPlugin;
 
 public class Hafnium extends NGPlugin {
 
 	@Override
 	public void load( NGApplication application ) {
-		registerComponents();
+		registerComponents( application );
 		application.routeTable().map( "/i/*", new ObjectRouteHandler() );
 	}
 
-	public static void registerComponents() {
-		NGElementUtils.addClass( USBaseComponent.class );
-		NGElementUtils.addClass( USBaseViewTools.class );
-		NGElementUtils.addClass( USEditPageGeneric.class );
-		NGElementUtils.addClass( USEditWrapper.class );
-		NGElementUtils.addClass( USListPageEdit.class );
-		NGElementUtils.addClass( USViewPage.class );
-		NGElementUtils.addClass( USViewPageGeneric.class );
-		NGElementUtils.addClass( USViewWrapper.class );
-		NGElementUtils.addClass( ViewLink.class );
+	public static void registerComponents( NGApplication application ) {
+		application.elementManager().registerElementClass( USBaseComponent.class );
+		application.elementManager().registerElementClass( USBaseViewTools.class );
+		application.elementManager().registerElementClass( USEditPageGeneric.class );
+		application.elementManager().registerElementClass( USEditWrapper.class );
+		application.elementManager().registerElementClass( USListPageEdit.class );
+		application.elementManager().registerElementClass( USViewPage.class );
+		application.elementManager().registerElementClass( USViewPageGeneric.class );
+		application.elementManager().registerElementClass( USViewWrapper.class );
+		application.elementManager().registerElementClass( ViewLink.class );
 	}
 }
