209ccc7d874183be6a78259b859e1e3120826789
Hugi Thordarson <hugi@karlmenn.is>
2026-03-03 15:17:54 +0000

    Add pluggable access control to view tools
    USBaseViewTools now accepts a Predicate<NGContext> via setAccessCheck()
    to determine visibility. View tools are hidden by default until the
    consumer app registers an access check.

--- src/main/java/hafnium/components/USBaseViewTools.java
+++ src/main/java/hafnium/components/USBaseViewTools.java
@@ -1,10 +1,30 @@
 package hafnium.components;
 
+import java.util.function.Predicate;
+
 import ng.appserver.NGContext;
 
 public class USBaseViewTools extends USViewPage {
 
+	/**
+	 * Predicate that determines if the view tools should be shown for the current request context.
+	 * If null, view tools are always hidden.
+	 */
+	private static Predicate<NGContext> _accessCheck;
+
 	public USBaseViewTools( NGContext context ) {
 		super( context );
+	}
+
+	/**
+	 * Set the access check predicate. The predicate receives the current NGContext
+	 * and should return true if the view tools should be shown.
+	 */
+	public static void setAccessCheck( Predicate<NGContext> accessCheck ) {
+		_accessCheck = accessCheck;
+	}
+
+	public boolean showViewTools() {
+		return _accessCheck != null && _accessCheck.test( context() );
 	}
 }
